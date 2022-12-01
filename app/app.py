5becbdb4afe4b03fb581fd1dac78fec2d58287a4
MayGi90 <lopezmayragisela@gmail.com>
2022-12-01 10:33:02 -0300

    Primera parte del proyecto

---
+++ app/app.py
@@ -0,0 +1,21 @@
+import flask
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+def home():
+
+
+    persona_titular = lista_de_datos['dni']
+    return render_template('home-banking.html', saludo=persona_titular.saludo(), movement=persona_titular.obtener_todos_los_movimientos())
+
+
+if __name__ == '__main__':
+   import proceso_cuentas
+   lista_de_datos = proceso_cuentas.crear_cuentas()
+   app.run(debug=True)   
+    
